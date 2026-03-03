37118fbb0cccf2e8829ad7e83f9bf54269a7b1b6
Saif Ul Haq <saifulhaq200@gmail.com>
2026-03-03 16:27:19 +0000

    Assignment 3

---
+++ Assignment_3_.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter a Character\n");
+    scanf("%c",&ch);
+    printf("The Ascii code of the charachter %c is %d\n",ch,ch);
+    return 0;
+}
